3195bfcd4509e5b21f2e5862fe48fa11083d9262
Roman Braginetz <animatorroma228@gmail.com>
2025-04-07 14:13:56 +0300

    Graph calculating has been added

--- Program.py
+++ Program.py
@@ -1,3 +1,5 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 from colorama import Fore, Style
 from colorama import init
 from tkinter import *
@@ -35,6 +37,8 @@ class Number:
         print(' ')
         print(Fore.GREEN + '4. Functional Calculating' + Style.RESET_ALL)
         print(Fore.GREEN + '5. Alternative Calculating' + Style.RESET_ALL)
+        print(Fore.GREEN + '6. Grap Calculating' + Style.RESET_ALL)
+        print(' ')
         choose = int(input("Choose calculating style "))
 
         if choose == 1:
@@ -51,6 +55,9 @@ class Number:
 
         elif choose == 5:
             self.alternative_calculating()
+
+        elif choose == 6:
+            self.graph_calculating()
 
     # limited calculating logic
 
@@ -138,6 +145,39 @@ class Number:
                 logging.info('Functional calculating has been ended corrected')
                 logging.info(f'User entres {self.first_number} + {self.second_number}. Answer: {result}')
 
+        except ValueError:
+            logging.error('Value error has been happened')
+
+    def graph_calculating(self):
+        try:
+            self.first_number = int(input())
+            self.second_number = int(input())
+            result = self.first_number + self.second_number
+
+            result_advance = result
+            result_advance -= random.randrange(0,99)
+
+            G = nx.DiGraph()
+
+            G.add_edge("A", "B", weight=self.first_number)
+            G.add_edge("C", "D", weight=self.second_number)
+            G.add_edge("D", "B", weight=result)
+            G.add_edge("A", "D", weight=result_advance)
+
+            for u, v, data in G.edges(data=True):
+                print(f"Edge: {u} -> {v} has weight {data['weight']}")
+
+            pos = nx.spring_layout(G)
+
+            nx.draw(G, pos, with_labels=True, node_color="grey", edge_color="blue")
+
+            edge_labels = nx.get_edge_attributes(G, "weight")
+
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+            plt.show()
+            logging.info('Graph calculating has been ended corrected')
+            logging.info(f'User entres {self.first_number} + {self.second_number}. Answer: {result}, Extra answer: {result_advance}')
         except ValueError:
             logging.error('Value error has been happened')
 
